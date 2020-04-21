4a672c12a8aadc9c9a1af30c4a9c33bc61b274ab
Ales Puchilo <ales.puchilo@gmail.com>
2020-04-21 16:27:56 +0300

    Add progress bar and outputs, gitignore upd

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from tx_generator import *
 from config import *
 import arxiv
 import json
+from progressbar import Bar, ETA, FileTransferSpeed, Percentage, ProgressBar
 
-results = arxiv.query(query=QUERY, max_results=2)
+widgets = ['Progress: ', Percentage(), ' ',
+               Bar(marker='-',left='[',right=']'),
+               ' ', ETA(), ' ', FileTransferSpeed()]
+pbar = ProgressBar(widgets=widgets, maxval=500)
 
+results = arxiv.query(query=QUERY, max_chunk_results=10, iterative=True)
+
+print('Collecting articles by', QUERY, 'keyword and generating transactions for broadcasting.')
 i = 0
-for result in results:
+for result in pbar(results()):
     df = create_clinks_set(result, DATA_PATH)
     tx = tx_generator(df, 'quantum computing article')
     with open(TXS_PATH + '/link_tx{}.json'.format(i), 'w') as outfile:
